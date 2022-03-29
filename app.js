566d63d1538f0fac994fb15dee34467f1cb10008
Belendia Serda <belendia@gmail.com>
2022-03-29 11:02:31 +0300

    Add middleware to parse post body and handle get and post requests

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+//middleware
+app.use(bodyParser.urlencoded());
+
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next(); //Allows the request to continue to the next middleware in line
+});
+
+app.get("/add-product", (req, res, next) => {
+  console.log("In another middleware");
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
+});
+
+app.post("/product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
 app.get("/", function (req, res) {
-  res.send("Hello World");
+  res.send("<h1>Hello World!</h>");
 });
 
 app.listen(3000);
