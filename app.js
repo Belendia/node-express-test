305ae8c6316412ad6518b1314cd8ae633532ada5
Belendia Serda <belendia@gmail.com>
2022-03-29 16:17:43 +0300

    Serve 404.html page

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -20,7 +22,7 @@ app.use(shopRoutes);
 
 //catch all middleware for 404
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
