96efd5816eec4d3afb6e2c25076307e93e0c34f9
Belendia Serda <belendia@gmail.com>
2022-03-29 15:01:26 +0300

    Add routes

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
 //middleware
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
   console.log("In the middleware");
   next(); //Allows the request to continue to the next middleware in line
 });
 
-app.get("/add-product", (req, res, next) => {
-  console.log("In another middleware");
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post("/product", (req, res) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h>");
-});
+// the route can also be registed as a middleware
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h>");
+});
+
+module.exports = router;
