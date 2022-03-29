d67151ff3f95c907ad4199a98a60a797254f8ade
Belendia Serda <belendia@gmail.com>
2022-03-29 15:43:17 +0300

    Serve HTML pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
@@ -5,9 +7,7 @@ const router = express.Router();
 // this route is reached using /admin/add-products GET request
 router.get("/add-product", (req, res, next) => {
   console.log("In another middleware");
-  res.send(
-    '<form action="/admin/add-product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 // this route is reached using /admin/add-products POST request
