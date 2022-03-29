93be06c4fc301c6cbfb924da4442f4475e6a38b9
Belendia Serda <belendia@gmail.com>
2022-03-29 16:33:43 +0300

    Use helper function for nagivation

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 const express = require("express");
 
+const rootDir = require("../utils/path");
+
 const router = express.Router();
 
 // this route is reached using /admin/add-products GET request
 router.get("/add-product", (req, res, next) => {
   console.log("In another middleware");
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
 // this route is reached using /admin/add-products POST request
