998ffc9d61c544d9cc58524e2a9b0f596748a6e7
Belendia Serda <belendia@gmail.com>
2022-05-09 14:57:55 +0300

    Implement update product

--- controllers/admin.js
+++ controllers/admin.js
@@ -25,41 +25,57 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-// exports.getProducts = (req, res, next) => {
-//   Product.findAll({ where: { userId: req.user.id } }).then((products) => {
-//     res.render("admin/products", {
-//       prods: products,
-//       hasProducts: products.length > 0,
-//       pageTitle: "Admin products",
-//       path: "/admin/products",
-//     }); //it use shop.pug by default;
-//   });
-// };
+exports.getProducts = (req, res, next) => {
+  //{ where: { userId: req.user.id } }
+  Product.fetchAll().then((products) => {
+    res.render("admin/products", {
+      prods: products,
+      hasProducts: products.length > 0,
+      pageTitle: "Admin products",
+      path: "/admin/products",
+    }); //it use shop.pug by default;
+  });
+};
 
-// exports.postEditProduct = (req, res) => {
-//   const id = req.body.id;
-//   const title = req.body.title;
-//   const imageURL = req.body.imageURL;
-//   const price = req.body.price;
-//   const description = req.body.description;
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
 
-//   // const product = new Product(id, title, imageURL, description, price);
-//   // product.save();
-//   Product.findByPk(id)
-//     .then((product) => {
-//       product.title = title;
-//       product.imageURL = imageURL;
-//       product.price = price;
-//       product.description = description;
+  const productId = req.params.productId;
 
-//       return product.save();
-//     })
-//     .then((result) => {
-//       console.log("Updated Products");
-//       res.redirect("/admin/products");
-//     })
-//     .catch((err) => console.log(err));
-// };
+  Product.findById(productId).then((product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
+
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+  const imageURL = req.body.imageURL;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(title, price, description, imageURL, id);
+
+  product
+    .save()
+    .then((result) => {
+      console.log("Updated Products");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
+};
 
 // exports.postDeleteProduct = (req, res, next) => {
 //   const id = req.body.id;
@@ -72,26 +88,4 @@ exports.getAddProduct = (req, res, next) => {
 //       res.redirect("/admin/products");
 //     })
 //     .catch((err) => console.log(err));
-// };
-
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect("/");
-//   }
-
-//   const productId = req.params.productId;
-
-//   Product.findByPk(productId).then((product) => {
-//     if (!product) {
-//       return res.redirect("/");
-//     }
-
-//     res.render("admin/edit-product", {
-//       pageTitle: "Edit Product",
-//       path: "/admin/edit-product",
-//       editing: editMode,
-//       product: product,
-//     });
-//   });
 // };
--- models/product.js
+++ models/product.js
@@ -2,18 +2,30 @@ const mongodb = require("mongodb");
 const getDb = require("../utils/db").getDb;
 
 class Product {
-  constructor(title, price, description, imageURL) {
+  constructor(title, price, description, imageURL, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageURL = imageURL;
+    if (id) {
+      this._id = new mongodb.ObjectId(id);
+    }
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection("products")
-      .insertOne(this)
+    let dbOp;
+    if (this._id) {
+      // Update the product
+      dbOp = db
+        .collection("products")
+        .updateOne({ _id: this._id }, { $set: this });
+    } else {
+      // Create new product
+      dbOp = db.collection("products").insertOne(this);
+    }
+
+    return dbOp
       .then((result) => {
         console.log(result);
       })
