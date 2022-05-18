d120380b3ba664850bb39a5252c9925c15ea82d5
Belendia Serda <belendia@gmail.com>
2022-05-18 15:51:04 +0300

    Add route protection logic

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const authController = require("../controllers/auth");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
-router.post("/logout", authController.postLogout);
+router.post("/logout", isAuth, authController.postLogout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
 
